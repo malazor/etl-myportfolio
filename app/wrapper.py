1ed0f9ca60f3d9047c02934bdb1db64eaba8461f
Manuel A. Lazo (Desktop) <manuel.antonio.lazo@gmail.com>
2025-09-15 14:26:14 -0500

    Modificacion Wrapper

--- app/wrapper.py
+++ app/wrapper.py
@@ -1,6 +1,8 @@
 import subprocess
 import config
 import time
+import sys  # (asegúrate de tenerlo arriba del archivo)
+
 from app.core.logging_config import get_logger
 
 # lista de símbolos
@@ -10,12 +12,15 @@ logger.info("Inicio del job de carga histórica")
 
 for symbol in symbols:
     logger.info(f"Ejecutando main_symbol.py con {symbol}...")
-    subprocess.run(["python","-m", "app.main_symbol", "--symbols", symbol])
+#    subprocess.run(["python","-m", "app.main_symbol", "--symbols", symbol])
+    subprocess.run([sys.executable, "-m", "app.main_symbol", "--symbols", symbol])
+
     time.sleep(2)
 
 for symbol in symbols:
     logger.info(f"Ejecutando main_history.py con {symbol}...")
-    subprocess.run(["python","-m", "app.main_history", "--symbols", symbol])
+    # subprocess.run(["python","-m", "app.main_history", "--symbols", symbol])
+    subprocess.run([sys.executable, "-m", "app.main_symbol", "--symbols", symbol])
     time.sleep(2)
 
 logger.info("Fin del job de carga histórica")
